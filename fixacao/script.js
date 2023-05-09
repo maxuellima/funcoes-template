3ae846711fce9d9704a41bb56c178c8ef7f9eee8
Maxuel Lima <maxuellima20@gmail.com>
2023-05-08 21:53:12 -0300

    Exercicios respondeidos em aula

--- fixacao/script.js
+++ fixacao/script.js
@@ -1,1 +1,7 @@
 // Resolva o Exercício de Fixação!
+const verificaIdade = (nome, anoNascimento, anoAtual) =>{
+    const verificando = anoAtual - anoNascimento
+    return (`${nome} é maior de idade? ${verificando >= 18}`)
+}
+console.log(verificaIdade("Sartre", 1993, 2023))
+console.log(verificaIdade("Socorro", 2019, 2023))
--- pratica-guiada/script.js
+++ pratica-guiada/script.js
@@ -1,1 +1,52 @@
 // Acompanhe a Prática Guiada e repita o exercício no Momento de Réplica!
+
+//Function tradicional
+function imprimirNome(){
+    //bloco de código
+    console.log("Olá Mundo!")
+}
+imprimirNome()
+
+//Expressão de função
+const nominho = function(imprimirNomes){
+    console.log(`Diga seu apelido: ${imprimirNomes}`)
+}
+nominho("Suel")
+nominho("Coco")
+nominho("Dani")
+
+
+//Function Arrow
+const nomear = (nome) =>{
+    console.log(`Meu nome é ${nome}!`)
+}
+nomear("Maxuel")
+nomear("Socorro")
+nomear("Sartre")
+
+//---------------------------->
+const fazerOperacoes = (numero) =>{
+    const verifica = numero % 2 === 0
+    const soma = numero + 10
+    const multiplica = numero * numero
+    return (`O número ${numero} é par? ${verifica}. Somado com 10  o resultado é ${soma}.
+    Multiplicado por ele mesmo o resultado é ${multiplica}`)
+}
+console.log(fazerOperacoes(12))
+
+//-------------------->
+/*const verificaDados = (login, senha) =>{
+    loginArmazenado = "max.@gmail.com"
+    senhaArmazenada = "123456"
+    
+    return loginArmazenado === login && senhaArmazenada === senha
+}
+console.log(verificaDados("max.@gmail.com", "123456"))*/
+
+const verificarDados = (login, senha) =>{
+    loginArmazenado = "meulogin@gmail.com"
+    senhaArmazenada = "12345"
+
+    return loginArmazenado === login && senhaArmazenada && senha
+}
+console.log(verificarDados("meug.amil", "3512"))
